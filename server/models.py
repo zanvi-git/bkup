2d3abd726d50e618aced8f9c34f0550bb8ee2f8c
zanvi-git <avijaileo@gmail.com>
2026-01-05 13:29:12 +0530

    add models

---
+++ server/models.py
@@ -0,0 +1,29 @@
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+import datetime
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    quota_bytes = db.Column(db.BigInteger, default=1024 * 1024 * 1024) # 1GB default
+    used_bytes = db.Column(db.BigInteger, default=0)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "quota_bytes": self.quota_bytes,
+            "used_bytes": self.used_bytes,
+            "created_at": self.created_at.isoformat()
+        }
